4f9369edb6ae57a685d45a85737477deaca29676
user name <davidchandrachang@gmail.com>
2023-02-24 13:27:04 +0700

    first commit

---
+++ resources/views/aspirasi.blade.php
@@ -0,0 +1,205 @@
+@extends('layouts.main')
+@section('container')
+
+<section id="input" style="height: auto;">
+    <div class="row d-flex  justify-content-center">
+        <div class="col-12 my-4 bg-light px-5 py-3 rounded">
+            <a class="navbar-brand fw-bold text-primary fs-3" href="/aspirasi">Input Pengaduan</a>
+        </div>
+        <div class="col-sm-12 col-md-8 col-lg-6">
+            @if (request('id_pelaporan') != null)
+            <div class="alert mt-3 alert-warning alert-dismissible fade show" role="alert">
+                <strong>Terimakasih Telah Melakukan Pengaduan <br>
+                    Nomor Pengaduan : {{ request('id_pelaporan') }}</strong><br>
+                <small class="">Silahkan Di Ingat Nomor pengaduannya!!</small>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+          
+            @endif
+            @if (request('nis') != null)
+            <div class="alert mt-3 alert-danger alert-dismissible fade show" role="alert">
+               <strong> NIS Anda Belum Terdaftar!! </strong><br>
+               <small>Silahkan Isi Datanya Kembali Dengan Benar</small>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+          
+            @endif
+            <div class="card mb-5 shadow">
+                <div class="card-body p-5">
+                    <form action="/aspirasi/store" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <label class="form-label fw-bold">ID Pelapor</label>
+                            <input type="text" name="id_pelaporan" class="form-control bg-primary text-light fw-bold" readonly
+                                value="{{ $no }}">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label fw-bold">Nomor Induk Kependudukan</label>
+                            <input type="number" name="nis" value="{{ old('nis') }}"
+                                class="form-control @error('nis') is-invalid @enderror">
+                            @error('nis')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label fw-bold">Kategori</label>
+                            <select id="inputState" class="form-select" name="id_kategori" id="id_kategori1">
+                            <option selected disabled>Choose...</option>
+                            @foreach ($kategori as $kat)
+                            <option value="{{ $kat->id_kategori }}">{{ $kat->ket_kategori }}</option>
+                            @endforeach
+                            </select>
+                            {{-- <div class=" container row d-flex justify-content-center">
+                                <div class="col-12 bg-warning rounded-4 bg-gradient p-3">
+                                    <div class="row ">
+                                        @foreach ($kategori as $kat)
+                                        <div class="col-sm-12 col-lg-4 col-md-12 ">
+                                            <input class="form-check-input" value="{{ $kat->id_kategori }}" type="radio"
+                                                name="id_kategori" id="id_kategori1">
+                                            <label class="form-check-label" for="id_kategori1">
+                                                {{ $kat->ket_kategori }}
+                                            </label>
+                                        </div>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            </div> --}}
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label fw-bold">Lokasi</label>
+                            <input type="text" name="lokasi" value="{{ old('lokasi') }}"
+                                class="form-control  @error('lokasi') is-invalid @enderror">
+                            @error('lokasi')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label fw-bold">Keterangan</label>
+                            <textarea name="ket" id="" value="{{ old('ket') }}"
+                                class="form-control @error('ket') is-invalid @enderror" rows="2"></textarea>
+                            @error('ket')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="image" class="form-label fw-bold">Upload Gambar</label>
+                            <input class="form-control @error('ket') is-invalid @enderror" type="file" id="image" name="image">
+                            @error('image')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                          </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+          
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="aspirasi" class="justify-content-center" style="height: auto;">
+    <div class="row justify-content-center">
+        <div class="col-12 my-4 bg-light px-5 py-3 rounded">
+            <a class="navbar-brand fw-bold text-primary fs-3" href="/aspirasi">Lihat Pengaduan</a>
+        </div>
+        @if (request('search') == null)
+        <div class="col-8 pb-3">
+            <form action="/#aspirasi" class="" method="get">
+                <label class="form-label fw-bold">Nomor Pengaduan</label>
+                <div class="input-group">
+                    <input type="text" required name="search" value="{{ request('search') }}"
+                        class="form-control" placeholder="Masukkan Nomor Pengaduan"
+                        aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2"><i
+                            class="bi bi-search"></i></button>
+                </div>
+            </form>
+        </div>
+        @endif
+        @if (request('search') != null)
+        <div class="col-4 pb-3">
+            <form action="/#aspirasi" class="" method="get">
+                <label class="form-label fw-bold">Nomor Pengaduan</label>
+                <div class="input-group">
+                    <input type="text" required name="search" value="{{ request('search') }}"
+                        class="form-control" placeholder="Masukkan Nomor Pengaduan"
+                        aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2"><i
+                            class="bi bi-search"></i></button>
+                </div>
+            </form>
+        </div>
+        @endif
+        <div class="col-8"> 
+            @if (request('search') != null)
+            <div class="card text-center">
+                @foreach ($aspirasi as $as)
+                <div class="card-header">
+                    <div class="d-flex justify-content-between">
+                        <div class="nomor bg-danger p-2 rounded text-light"><span class="fw-bold">Nomor Pengaduan : </span>{{ $as->id_aspirasi }}</div>
+                    </div>
+                </div>
+                <div class="card-body shadow">
+                    <div class="d-flex justify-content-center mb-3">
+                        <div class="gambar mx-3 {{ ($as->input_aspirasi->image === null ? 'd-none' : 'd-block' )}}">
+                                <img src="{{ asset('storage/'. $as->input_aspirasi->image) }}" alt="" width="200px" class="rounded">
+                        </div>
+                        <div class="keterangan mx-3">
+                            <div class="d-flex border-bottom mb-2">
+                                <p class="fw-bold p-0 m-0 me-2">Kategori : </p>
+                                <p class="p-0 m-0 text-danger fw-bold">{{ $as->kategori->ket_kategori }}</p>
+                            </div>
+                            <div class="d-flex border-bottom mb-2">
+                                <p class="fw-bold p-0 m-0 me-2">Status : </p>
+                                <p class="p-0 m-0">{{ $as->status }}</p>
+                            </div>
+                            <div class="d-flex border-bottom mb-2">
+                                <p class="fw-bold p-0 m-0 me-2">Alamat : </p>
+                                <p class="p-0 m-0">{{ $as->input_aspirasi->lokasi }}</p>
+                            </div>
+                            <div class="d-flex border-bottom mb-2">
+                                <p class="fw-bold p-0 m-0 me-2">Keterangan : </p>
+                                <p class="p-0 m-0">{{ $as->input_aspirasi->ket }}</p>
+                            </div>
+                            @if ($as['feedback'] != null)
+                            <div class="d-flex border-bottom mb-2">
+                                <p class="fw-bold p-0 m-0 me-2">Feedback : </p>
+                                <p class="p-0 m-0">{{ $as->feedback }} <i class="bi bi-star-fill"></i></p>
+                            </div>
+                            @endif
+                        </div>
+                    </div>
+                    @if ($as['status'] == 'Selesai' and $as['feedback'] == null)
+                    <h6 class="p-0 m-0 fw-bold text-primary">Rating</h6>
+                            <form action="/aspirasi/feedback" method="POST" class=" p-2 rounded-2 text-center">
+                                @csrf
+                                <input type="hidden" name="id_aspirasi" value="{{ $as->id_aspirasi  }}">
+                                <div class="btn-group" role="group" aria-label="Basic example">
+                                    <button type="radio" class="btn btn-outline-danger" required name="feedback" value="1" >1 <i class="bi bi-star-fill"></i></button>
+                                    <button type="radio" class="btn btn-outline-danger" name="feedback" required value="2">2 <i class="bi bi-star-fill"></i></button>
+                                    <button type="radio" class="btn btn-outline-primary" name="feedback" required value="3">3 <i class="bi bi-star-fill"></i></button>
+                                    <button type="radio" class="btn btn-outline-primary" name="feedback" required value="4">4 <i class="bi bi-star-fill"></i></button>
+                                    <button type="radio" class="btn btn-outline-success" name="feedback" required value="5">5 <i class="bi bi-star-fill"></i></button>
+                                </div>
+                            </form>
+                            @endif
+                </div>
+                <div class="card-footer text-muted">
+                    {{ $as->input_aspirasi->created_at }}
+                </div>
+            </div>
+            @endforeach
+            @else
+            @endif
+        </div>
+    </div>
+</section>
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,63 @@
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container">
+    <a class="navbar-brand text-primary fw-bold fs-3" href="/">
+      Pengaduan Siswa
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link fs-5 {{ ($title === "Pengaduan" ? 'active fw-bold' : '' )}}" aria-current="page" href="/">Aspirasi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fs-5 {{ ($title === "About" ? 'active fw-bold' : '' )}}" aria-current="page" href="/about">About</a>
+        </li>
+      </ul>
+      <a class="navbar-brand text-primary fw-bold fs-3" href="" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <i class="bi bi-bookmark-star-fill d-inline-block align-text-top "></i>
+      </a>
+    </div>
+  </div>
+</nav>
+
+  <div class="container">  @if (session()->has('LoginError'))
+    <div class="alert alert-danger my-3 alert-dismissible fade show" role="alert">
+      {{ session('LoginError') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif</div>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Login Admin</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form action="/login" method="post">
+            @csrf
+            <div class="form-floating mb-3">
+              <input type="text" value="{{ old('username') }}" class="form-control @error('username') is-invalid @enderror" autofocus name="username"  id="floatingInput" placeholder="username" >
+              <label for="floatingInput">Username</label>
+              @error('username')
+                  <div class="invalid-feedback">{{ $message }}</div>
+              @enderror
+            </div>
+            <div class="form-floating">
+              <input type="password" class="form-control @error('password') is-invalid @enderror" autofocus name="password" id="floatingPassword" placeholder="Password">
+              <label for="floatingPassword">Password</label>
+              @error('password')
+              <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+        </div>
+      </div>
+    </div>
+  </div>
